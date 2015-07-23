1830a244c7a6e7f04439a3bf8cedeb7e9d51f759
kepennar <kevin.pennarun@gmail.com>
2015-07-23 13:23:07 +0200

    fixes

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import MessageApp from './components/MessageApp';
 
-window.React = React;
 const mountNode = document.getElementById("app");
-console.log('before render');
 React.render(<MessageApp />, mountNode);
